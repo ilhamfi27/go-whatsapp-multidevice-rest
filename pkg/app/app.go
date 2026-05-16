81327bcc0d6267e57922a25a70357244711e0976
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2026-05-16 16:07:16 +0700

    refactor: remove database package and related code, simplify app initialization

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"time"
 
-	"github.com/dimaskiddo/go-whatsapp-multidevice-rest/pkg/app/database"
 	"github.com/dimaskiddo/go-whatsapp-multidevice-rest/pkg/app/http"
 	"github.com/dimaskiddo/go-whatsapp-multidevice-rest/pkg/env"
 )
@@ -12,25 +11,11 @@ import (
 var (
 	AppWebhookURL       string
 	AppWebhookBasicAuth string
-	AppDatabase         *database.DatabaseContainer
 	AppRequest          *http.HttpClient
 )
 
 func init() {
 	var err error
-
-	dbType, err := env.GetEnvString("WHATSAPP_DATASTORE_TYPE")
-	if err != nil {
-		log.Fatal("Error Parse Environment Variable for Application Datastore Type")
-	}
-
-	dbURI, err := env.GetEnvString("WHATSAPP_DATASTORE_URI")
-	if err != nil {
-		log.Fatal("Error Parse Environment Variable for Application Datastore URI")
-	}
-
-	// Initialize App Client Datastore
-	initDB(dbType, dbURI)
 
 	appWebhookUrl, err := env.GetEnvString("APP_WEBHOOK_URL_TARGET")
 	if err != nil {
@@ -46,15 +31,6 @@ func init() {
 
 	// Initialize App HTTP Request
 	initHttpRequest()
-}
-
-func initDB(dbType string, dbURI string) {
-	// Initialize App Client Datastore
-	appDb, err := database.New(dbType, dbURI)
-	if err != nil {
-		log.Fatal("Error Connect Application Datastore: ", err)
-	}
-	AppDatabase = appDb
 }
 
 func initHttpRequest() {
